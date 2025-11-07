55324cacee569a97eba233adad347ec82f70d318
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-11-07 18:23:35 +0800

    feat: btnLogin onClick method implementation

--- LabEquipmentSystemForms/Login_Admin.cs
+++ LabEquipmentSystemForms/Login_Admin.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DataHelper;
 
 namespace LabEquipmentSystemForms
 {
@@ -15,6 +16,27 @@ namespace LabEquipmentSystemForms
         public FormLoginAdmin()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            bool success = DataAccess.LoginAdmin(
+                txtAdminId.Text.ToString(), 
+                txtPassword.Text.ToString());
+
+            if (success)
+            {
+                MessageBox.Show("Login Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                FormAdminHome adminHome = new FormAdminHome();
+                adminHome.Show();
+                this.Close();
+                adminHome.Focus();
+            } else
+            {
+                MessageBox.Show("Login Failed.", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtAdminId.Focus();
+            }
         }
     }
 }
