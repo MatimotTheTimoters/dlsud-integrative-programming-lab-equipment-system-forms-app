58ecc68daeb2965f72144d86edfc93d7ec126582
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-11-09 10:57:50 +0800

    feat: form design template

---
+++ LabEquipmentSystemForms/Admin_ProcessEquipmentRequests.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace LabEquipmentSystemForms
+{
+    partial class FormAdminProcessEquipmentRequests
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblEquipmentID = new System.Windows.Forms.Label();
+            this.txtEquipmentId = new System.Windows.Forms.TextBox();
+            this.lblStudentID = new System.Windows.Forms.Label();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.nudQuantity = new System.Windows.Forms.NumericUpDown();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnApprove = new System.Windows.Forms.Button();
+            this.btnDeny = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantity)).BeginInit();
+            this.flowLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.nudQuantity, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblQuantity, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtStudentID, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblStudentID, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtEquipmentId, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblEquipmentID, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView, 0, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(884, 461);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lblEquipmentID
+            // 
+            this.lblEquipmentID.AutoSize = true;
+            this.lblEquipmentID.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblEquipmentID.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEquipmentID.Location = new System.Drawing.Point(3, 0);
+            this.lblEquipmentID.Name = "lblEquipmentID";
+            this.lblEquipmentID.Size = new System.Drawing.Size(215, 46);
+            this.lblEquipmentID.TabIndex = 1;
+            this.lblEquipmentID.Text = "EQUIPMENT ID";
+            this.lblEquipmentID.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtEquipmentId
+            // 
+            this.txtEquipmentId.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtEquipmentId.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEquipmentId.Location = new System.Drawing.Point(224, 3);
+            this.txtEquipmentId.Name = "txtEquipmentId";
+            this.txtEquipmentId.ReadOnly = true;
+            this.txtEquipmentId.Size = new System.Drawing.Size(657, 43);
+            this.txtEquipmentId.TabIndex = 2;
+            // 
+            // lblStudentID
+            // 
+            this.lblStudentID.AutoSize = true;
+            this.lblStudentID.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblStudentID.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentID.Location = new System.Drawing.Point(3, 46);
+            this.lblStudentID.Name = "lblStudentID";
+            this.lblStudentID.Size = new System.Drawing.Size(215, 46);
+            this.lblStudentID.TabIndex = 3;
+            this.lblStudentID.Text = "STUDENT ID";
+            this.lblStudentID.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtStudentID.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudentID.Location = new System.Drawing.Point(224, 49);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.ReadOnly = true;
+            this.txtStudentID.Size = new System.Drawing.Size(657, 43);
+            this.txtStudentID.TabIndex = 4;
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblQuantity.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantity.Location = new System.Drawing.Point(3, 92);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(215, 46);
+            this.lblQuantity.TabIndex = 5;
+            this.lblQuantity.Text = "QUANTITY";
+            this.lblQuantity.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nudQuantity
+            // 
+            this.nudQuantity.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.nudQuantity.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.nudQuantity.Location = new System.Drawing.Point(224, 95);
+            this.nudQuantity.Name = "nudQuantity";
+            this.nudQuantity.ReadOnly = true;
+            this.nudQuantity.Size = new System.Drawing.Size(657, 43);
+            this.nudQuantity.TabIndex = 7;
+            this.nudQuantity.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.btnApprove);
+            this.flowLayoutPanel2.Controls.Add(this.btnDeny);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(224, 141);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(657, 40);
+            this.flowLayoutPanel2.TabIndex = 9;
+            // 
+            // btnApprove
+            // 
+            this.btnApprove.AutoSize = true;
+            this.btnApprove.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnApprove.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApprove.Location = new System.Drawing.Point(3, 3);
+            this.btnApprove.Name = "btnApprove";
+            this.btnApprove.Size = new System.Drawing.Size(144, 39);
+            this.btnApprove.TabIndex = 5;
+            this.btnApprove.Text = "APPROVE";
+            this.btnApprove.UseVisualStyleBackColor = true;
+            // 
+            // btnDeny
+            // 
+            this.btnDeny.AutoSize = true;
+            this.btnDeny.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnDeny.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeny.Location = new System.Drawing.Point(153, 3);
+            this.btnDeny.Name = "btnDeny";
+            this.btnDeny.Size = new System.Drawing.Size(100, 39);
+            this.btnDeny.TabIndex = 6;
+            this.btnDeny.Text = "DENY";
+            this.btnDeny.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView, 2);
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView.Location = new System.Drawing.Point(3, 187);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(878, 271);
+            this.dataGridView.TabIndex = 10;
+            // 
+            // FormAdminProcessEquipmentRequests
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FormAdminProcessEquipmentRequests";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Process Requests";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantity)).EndInit();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblEquipmentID;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.Label lblStudentID;
+        private System.Windows.Forms.TextBox txtEquipmentId;
+        private System.Windows.Forms.NumericUpDown nudQuantity;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button btnApprove;
+        private System.Windows.Forms.Button btnDeny;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
